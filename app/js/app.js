70b97cb585b4b687e82996e307d774166309b888
Itay Radotzki <radotzki@gmail.com>
2014-10-31 09:58:48 +0200

    End of first hour

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.services', 'myApp.controllers', 'myApp.filters'])
+angular.module('myApp', ['myApp.services', 'myApp.controllers', 'myApp.filters', 'angularFileUpload'])
 
 .config(['$routeProvider', '$locationProvider', function($routeProvider, $location) {
 	$location.html5Mode(true).hashPrefix('!');
@@ -10,10 +10,6 @@ angular.module('myApp', ['myApp.services', 'myApp.controllers', 'myApp.filters']
 	when('/home', {
 		templateUrl: 'partials/home.html',
 		controller: 'HomeController'
-	})
-	.when('/callback', {
-		templateUrl:'partials/callback.html',
-		controller: 'CallbackController'
 	})
 	.otherwise({redirectTo: '/home'});
 }])
--- app/js/services.js
+++ app/js/services.js
@@ -3,9 +3,6 @@
 /* Services */
 
 angular.module('myApp.services', [])
-.value('instagramAuthUrl', 'https://instagram.com/oauth/authorize/?response_type=token' +
- '&client_id=' +'7ec7b39f3a4e4eba812017a561b155af' +
- '&redirect_uri=' + 'http://localhost:5000/callback')
 
 .factory('InstagramToken', [function() {
   var token;
